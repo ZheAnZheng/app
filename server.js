1486c9224e64b41b3f9ad68e554f846584176a96
nixon <a37007647@gmail.com>
2021-11-21 18:26:40 +0000

    add express-handlebars & handlebars-template

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express=require('express');
 const app= express();
 const mongoose =require("mongoose");
+const exhbs=require("express-handlebars")
+
 //monggose連接db server
 mongoose.connect("mongodb://mongo:27017/users",{useNewUrlParser: true ,useUnifiedTopology: true })
 //取得mongoose連線資料
 const db=mongoose.connection;
-
 db.on('error',()=>{
     console.log('db error')
 });
 db.once('open',()=>{
     console.log('db connected')
 })
+
+app.engine('hbs',exhbs({defaultLayout:'main',extname:".hbs"}));
+app.set("view engine","hbs");
+
+
 app.get('/',(req,res)=>{
-    res.send('server created')
+    res.render('login')
 })
+
 
 app.listen(3000,()=>{
     console.log(`http://localhost:3000`);
